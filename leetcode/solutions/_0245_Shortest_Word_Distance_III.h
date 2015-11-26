4f71fb7094f7d198658b36bb8769fee7eb246e75
Weiping HUANG <Nightonke@outlook.com>
2015-11-26 15:02:35 +0800

    Update 245.

--- leetcode/solutions/_0245_Shortest_Word_Distance_III.h
+++ leetcode/solutions/_0245_Shortest_Word_Distance_III.h
@@ -8,7 +8,23 @@
 
 class _0245_Shortest_Word_Distance_III {
 
+    class Solution {
+    public:
+        int shortestWordDistance(vector<string>& words,
+            string word1, string word2) {
 
+            int ans = words.size(), p = -1, q = -1;
+
+            for (int i = words.size() - 1; i >= 0; i--) {
+                if (word1 == words[i]) p = i;
+                if (p != -1 && q != -1 && p != q) ans = min(abs(p - q), ans);
+                if (word2 == words[i]) q = i;
+                if (p != -1 && q != -1 && p != q) ans = min(abs(p - q), ans);
+            }
+
+            return ans;
+        }
+    };
 
 };
 
