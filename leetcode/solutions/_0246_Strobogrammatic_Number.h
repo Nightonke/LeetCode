13c6d8ec6942d605a234e61d8b76aecfb86a4401
Weiping HUANG <Nightonke@outlook.com>
2015-11-27 10:51:27 +0800

    Update 246.

--- leetcode/solutions/_0246_Strobogrammatic_Number.h
+++ leetcode/solutions/_0246_Strobogrammatic_Number.h
@@ -8,7 +8,22 @@
 
 class _0246_Strobogrammatic_Number {
 
-
+    class Solution {
+    public:
+        bool isStrobogrammaticChar(char a, char b) {
+            return (a == '6' && b == '9')
+                   || (a == '9' && b == '6')
+                   || (a == '1' && b == '1')
+                   || (a == '8' && b == '8')
+                   || (a == '0' && b == '0');
+        }
+        bool isStrobogrammatic(string num) {
+            for (int i = num.size() / 2; i >= 0; i--)
+                if (!isStrobogrammaticChar(num[i], num[num.size() - i - 1]))
+                    return false;
+            return true;
+        }
+    };
 
 };
 
