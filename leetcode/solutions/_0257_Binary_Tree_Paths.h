fec75be45f146d398a4ed7b9b792c73bcd4e5b69
Weiping HUANG <Nightonke@outlook.com>
2016-01-06 23:19:14 +0800

    Update 257.

--- leetcode/solutions/_0257_Binary_Tree_Paths.h
+++ leetcode/solutions/_0257_Binary_Tree_Paths.h
@@ -8,7 +8,46 @@
 
 class _0257_Binary_Tree_Paths {
 
+    class Solution {
+    public:
+        vector<string> binaryTreePaths(TreeNode* root) {
+            road.clear();
+            answers.clear();
+            preOrder(root);
+            return answers;
+        }
+    private:
+        vector<int> road;
+        vector<string> answers;
 
+        void preOrder(TreeNode * root) {
+            if (!root) return;
+            road.push_back(root->val);
+            if (root->left) preOrder(root->left);
+            if (root->right) preOrder(root->right);
+            if (!root->left && !root->right) {
+                string answer = "";
+                for (int i = 0; i < road.size(); i++) {
+                    if (i > 0) answer += "->";
+                    answer += intToString(road[i]);
+                }
+                if (answer != "") answers.push_back(answer);
+            }
+            road.pop_back();
+        }
+
+        string intToString(int i) {
+            string answer = "";
+            bool negative = i < 0;
+            if (negative) i = -i;
+            do {
+                answer += i % 10 + '0';
+                i /= 10;
+            } while (i);
+            reverse(answer.begin(), answer.end());
+            return negative ? "-" + answer : answer;
+        }
+    };
 
 };
 
