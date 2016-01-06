0b93cfa8c916f82e54f41358ae12b14147b43e56
Weiping HUANG <Nightonke@outlook.com>
2016-01-07 00:23:46 +0800

    Update 263.

--- leetcode/solutions/_0263_Ugly_Number.h
+++ leetcode/solutions/_0263_Ugly_Number.h
@@ -8,7 +8,13 @@
 
 class _0263_Ugly_Number {
 
-
+    class Solution {
+    public:
+        bool isUgly(int num) {
+            for (int i = 2; i < 6 && num; i++)while (num % i == 0) num /= i;
+            return num == 1;
+        }
+    };
 
 };
 
