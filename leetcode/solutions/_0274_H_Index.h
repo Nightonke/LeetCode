2c15ce86649cd36d240206ba767bc9c4c05a6f1c
Weiping HUANG <Nightonke@outlook.com>
2016-01-08 15:10:26 +0800

    Update 274.

--- leetcode/solutions/_0274_H_Index.h
+++ leetcode/solutions/_0274_H_Index.h
@@ -8,7 +8,24 @@
 
 class _0274_H_Index {
 
-
+    class Solution {
+    public:
+        int hIndex(vector<int>& citations) {
+            int size = citations.size();
+            if (size == 0) return 0;
+            vector<int> posibleAnswer(size + 1);
+            for (int i = 0; i < size; i++) {
+                if (citations[i] > size) posibleAnswer[size]++;
+                else posibleAnswer[citations[i]]++;
+            }
+            int counter = 0;
+            for (int i = size; i >= 0; i--) {
+                counter += posibleAnswer[i];
+                if (counter >= i) return i;
+            }
+            return 0;
+        }
+    };
 
 };
 
